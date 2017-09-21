3d21e403d9743136197b84dec8a0fac5cd01fcb5
quadrant26 <quadrant26@163.com>
2017-09-21 11:41:23 +0800

    require css

--- docs/09-01/src/js/components/footer.js
+++ docs/09-01/src/js/components/footer.js
@@ -1,10 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var styleFooter = require('../../css/footer.css');
+
 export default class FooterComponent extends React.Component{
+
     render(){
+        
+        console.log(styleFooter);
+
         return (
-            <footer className="smallFooterFontSize">
+            <footer className="smallFooterFontSize" className={styleFooter.miniFooterColor}>
                 <h1>这是页面尾部的内容</h1>
             </footer>
         )
