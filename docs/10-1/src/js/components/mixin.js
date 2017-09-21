9b2d60a0e76fffafb8f515f5c64ce10eb2e59b1f
quadrant26 <quadrant26@163.com>
2017-09-21 15:45:14 +0800

    10-mixin

---
+++ docs/10-1/src/js/components/mixin.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactMixin = require('react-mixin');
+
+const MixinLog = {
+    log(){
+        console.log('abcdefg.....');
+    }
+}
+
+export default MixinLog;
