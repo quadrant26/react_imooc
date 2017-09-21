fae05eb91fa6f0692dbdfc1bd33d64fe1e62c044
quadrant26 <quadrant26@163.com>
2017-09-21 15:46:34 +0800

    10-DetailComponent

---
+++ docs/10-1/src/js/components/detail.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+export default class DetailComponent extends React.Component{
+    render(){
+        return (
+            <div>
+                <p>这是列表详情页</p>
+            </div>
+        )
+    }
+}
