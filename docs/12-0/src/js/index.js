0704d31ce5144297386912ef9ae1ac52c2370634
quadrant26 <quadrant26@163.com>
2017-09-28 10:40:59 +0800

    example init project

---
+++ docs/12-0/src/js/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class IndexComponent extends React.Component{
+    render() {
+        return (
+            <div>
+                Init
+            </div>
+        )
+    }
+}
