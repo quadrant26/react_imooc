9e141a9dcdaad5824615de6663ae1ca7eb0fdf36
quadrant26 <quadrant26@163.com>
2017-09-30 15:45:30 +0800

    update antd

--- docs/13-0/src/js/components/pc-header.js
+++ docs/13-0/src/js/components/pc-header.js
@@ -1,23 +1,111 @@
 import React from 'react';
 
 // use antd 栅格系统
-import { Row, Col } from 'antd';
-import { Menu, Icon } from 'antd';
+import { Modal, Button, Link, Form, Menu, Icon, Row, Col, Input, Checkbox, Tabs, message } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
 
 import 'antd/dist/antd';
 
-export default class PcHeaderComponent extends React.Component{
+class PcHeaderComponent extends React.Component{
 
     constructor(){
         super();
         this.state = {
-            current : "top"
+            current : "top",
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
         }
     }
 
+    // 设置模态框是否显示
+    setModalVisible (value){
+        this.setState({modalVisible: value});
+    }
+
+    handleClick (e){
+        // console.log(e);
+        if( e.key == "register" ){
+            this.setState({current: "register"});
+            this.setModalVisible (true);
+        }else{
+            {
+                this.setState({current: e.key})
+            }
+        }
+    };
+
+    // 提交
+    handleSubmit (e){
+
+        //页面开始向 API 进行提交数据
+        e.preventDefault();
+
+        var myFetchOptions = {
+			method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        var url;
+        
+        if( this.state.action == "login"){
+            url = 'http://localhost:8866/react_imooc/login.php?g_username='+ formData.g_username +'&g_password=' + formData.g_password;
+        }else{
+            url = 'http://localhost:8866/react_imooc/reg.php?r_username='+ formData.r_username +'&r_password=' + formData.r_password  +'&r_confrimPassword=' + formData.r_confrimPassword ;
+        };
+        
+        fetch(url)
+        .then( res => res.json())
+        .then( json => {
+            if( json.code === 1){
+                message.error(json.error_msg);
+            }else{
+                this.setModalVisible(false);
+                this.setState({
+                    userNickName : formData.g_username || formData.r_username,
+                    hasLogined: true,
+                    modalVisible: false
+                });
+            };
+        });
+    }
+
+    // 登录还是注册弹出框显示
+    callback (key){
+        if( key == 1){
+            this.setState({action: "login"});
+        }else{
+            this.setState({action: "register"});
+        }
+    }
+
+    // 退出登录
+    logout (){
+        this.setState({
+            userNickName : '',
+            hasLogined: false
+        })
+    }
+
     render () {
+        let { getFieldDecorator } = this.props.form;
+        const userShow = this.state.hasLogined ? 
+            <Menu.Item key="register" class="register">
+                <Button type="dashed">{this.state.userNickName}</Button>
+                &nbsp;&nbsp;
+                <Button type="dashed" htmlType="button">个人中心</Button>
+                &nbsp;&nbsp;
+                <Button type="primary" ghost htmlType="button" onClick={this.logout.bind(this)}>退出</Button>
+            </Menu.Item>
+            : 
+            <Menu.Item key="register" class="register">
+                <Icon type="appstore" />注册/登录
+            </Menu.Item>;
+
         return (
             <div>
                 <Row>
@@ -26,8 +114,8 @@ export default class PcHeaderComponent extends React.Component{
                         <img src="./src/images/logo.png" alt="logo" title="logo" />
                         <span>React News</span>
                     </Col>
-                    <Col span={12}>
-                        <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+                    <Col span={16}>
+                        <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
                             <Menu.Item key="top">
                                 <Icon type="appstore" />头条
                             </Menu.Item>
@@ -55,12 +143,64 @@ export default class PcHeaderComponent extends React.Component{
                             <Menu.Item key="fashion">
                                 <Icon type="appstore" />时尚
                             </Menu.Item>
+                            {userShow}
                         </Menu>
                     </Col>
-                    <Col span={4}></Col>
                     <Col span={2}></Col>
+                    <Modal title="用户中心" visible={this.state.modalVisible} onOk={() => this.setModalVisible(false)} onCancel={() => this.setModalVisible(false)} okText="关闭">
+                        <Tabs type="card" onChange={ this.callback.bind(this) }>
+                            <TabPane tab="登录" key="1">
+                                <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                    <FormItem label="账户">
+                                        {getFieldDecorator('g_username', {
+                                            rules: [{ required: true, message: 'Please input your username!' }],
+                                        })(
+                                            <Input  prefix={<Icon type="user" style={{ fontSize: 13 }} />} required placeholder="请输入您的账号"/>
+                                        )}
+                                    </FormItem>
+                                    <FormItem label="密码">
+                                        {getFieldDecorator('g_password', {
+                                            rules: [{ required: true, message: 'Please input your password!'}],
+                                        })(
+                                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} required type="password" placeholder="请输入您的密码"/>
+                                        )}
+                                    </FormItem>
+                                    <Button type="primary" htmlType="submit">登录</Button>
+                                </Form>
+                            </TabPane>
+                            <TabPane tab="注册" key="2">
+                                <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                                    <FormItem label="账户">
+                                        {getFieldDecorator('r_username', {
+                                            rules: [{ required: true, message: 'Please input your username!' }],
+                                        })(
+                                            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} required placeholder="请输入您的账号"/>
+                                        )}
+                                    </FormItem>
+                                    <FormItem label="密码">
+                                        {getFieldDecorator('r_password', {
+                                            rules: [{ required: true, message: 'Please input your password!' }],
+                                        })(
+                                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} required type="password" placeholder="请输入您的密码"/>
+                                        )}
+                                    </FormItem>
+                                    <FormItem label="确认密码">
+                                        {getFieldDecorator('r_confrimPassword', {
+                                            rules: [{ required: true, message: 'Please input your confrimPassword!' }],
+                                        })(
+                                            <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} required type="password" placeholder="请输入您的密码"/>
+                                        )}
+                                    </FormItem>
+                                    <Button type="primary" htmlType="submit">注册</Button>
+                                </Form>
+                            </TabPane>
+                        </Tabs>
+                    </Modal>
                 </Row>
             </div>
         )
     }
 }
+
+export default PcHeaderComponent = Form.create({})(PcHeaderComponent);
+    
