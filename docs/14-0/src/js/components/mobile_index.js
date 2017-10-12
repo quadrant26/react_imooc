7b4ddf0c6fe08ea3d4bd419309a3c9aae31be450
quadrant26 <quadrant26@163.com>
2017-10-12 10:54:11 +0800

    添加移动端的首页导航

--- docs/14-0/src/js/components/mobile_index.js
+++ docs/14-0/src/js/components/mobile_index.js
@@ -5,11 +5,21 @@ import ReactDom from 'react-dom';
 import MobileHeaderComponent from './mobile-header';
 import MobileFooterComponent from './mobile-footer';
 
+import { Tabs, Icon } from 'antd';
+const TabPane = Tabs.TabPane;
+
 export default class MobileIndexComponent extends React.Component{
     render(){
         return (
             <div>
                 <MobileHeaderComponent/>
+                <Tabs defaultActiveKey="1">
+                    <TabPane tab={<span><Icon type="appstore" />头条</span>} key="1">头条新闻</TabPane>
+                    <TabPane tab={<span><Icon type="appstore" />社会</span>} key="2">社会新闻</TabPane>
+                    <TabPane tab={<span><Icon type="appstore" />国内</span>} key="3">国内新闻</TabPane>
+                    <TabPane tab={<span><Icon type="appstore" />娱乐</span>} key="4">娱乐新闻</TabPane>
+                    <TabPane tab={<span><Icon type="appstore" />体育</span>} key="5">体育新闻</TabPane>
+                </Tabs>
                 <MobileFooterComponent/>
             </div>
         )
