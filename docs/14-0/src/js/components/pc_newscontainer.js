9d6bd809ac1a7d5b46486facab51120a10d0d24c
quadrant26 <quadrant26@163.com>
2017-10-12 14:14:48 +0800

    pc news container

--- docs/14-0/src/js/components/pc_newscontainer.js
+++ docs/14-0/src/js/components/pc_newscontainer.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Row, Col, Tabs, Carousel } from 'antd';
 const TabPane = Tabs.TabPane;
 
+import PCNewsBlockComponent from './pc_news_block';
+import PCNewsImageBlock from './pc_news_image_block';
+
 export default class PcNewContainComponent extends React.Component{
     render (){
         const setting = {
@@ -26,7 +29,16 @@ export default class PcNewContainComponent extends React.Component{
                                     <div><img src="./src/images/carousel_4.jpg"/></div>
                                 </Carousel>
                             </div>
+                            <PCNewsImageBlock  count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="112px"></PCNewsImageBlock>
                         </div>
+                        <Tabs defaultActiveKey="1" className="tabs_news">
+                            <TabPane tab="头条" key="1">
+                                <PCNewsBlockComponent type="top" count={20} width="100%" bordered="false" ></PCNewsBlockComponent>
+                            </TabPane>
+                            <TabPane tab="国际" key="2">
+                                <PCNewsBlockComponent type="guoji" count={20} width="100%" bordered="false" ></PCNewsBlockComponent>
+                            </TabPane>
+                        </Tabs>
                     </Col>
                     <Col span={2}></Col>
                 </Row>
