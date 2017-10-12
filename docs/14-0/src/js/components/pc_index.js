63c1404af468d394a72becbd35c8f64b3fda7931
quadrant26 <quadrant26@163.com>
2017-10-12 11:13:04 +0800

    add newsContainerComponent

--- docs/14-0/src/js/components/pc_index.js
+++ docs/14-0/src/js/components/pc_index.js
@@ -4,12 +4,14 @@ import ReactDom from 'react-dom';
 // 导入header && footer 模块
 import PcHeaderComponent from './pc-header';
 import PcFooterComponent from './pc-footer';
+import PcNewContainComponent from './pc_newscontainer'
 
 export default class PCIndexComponent extends React.Component{
     render(){
         return (
             <div>
                 <PcHeaderComponent/>
+                <PcNewContainComponent></PcNewContainComponent>
                 <PcFooterComponent/>
             </div>
         )
